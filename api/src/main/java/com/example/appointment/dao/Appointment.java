e204bc8c70d5eb17c819129edbae4066e800b37e
NakosPTI <nakos.pti@gmail.com>
2025-08-11 04:50:57 +0200

    MongoDb implementation

--- api/src/main/java/com/example/appointment/dao/Appointment.java
+++ api/src/main/java/com/example/appointment/dao/Appointment.java
@@ -1,83 +1,20 @@
 package com.example.appointment.dao;
 
+<<<<<<< HEAD
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+=======
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.MappedEntity;
+import jakarta.validation.constraints.NotBlank;
+>>>>>>> f67e42c (MongoDb implementation)
 
-/*
 @MappedEntity
 public record Appointment(@Id String id,
                           @NotBlank String userId,
                           @NotBlank String appointmentTime,
                           AppointmentStatusInfo statusInfo) {
 
-}
-*/
-
-//H2 temp implementation
-@Entity
-@Table(name = "appointments")
-public class Appointment {
-
-    @Id
-    String appointmentId;
-    @Column
-    String userId;
-    @Column
-    String appointmentTime;
-    @Column
-    String appointmentStatus;
-    @Column
-    String statusLastModified;
-    @Column
-    String statusChangeComment;
-
-    public String getAppointmentId() {
-        return appointmentId;
-    }
-
-    public void setAppointmentId(String appointmentId) {
-        this.appointmentId = appointmentId;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getAppointmentTime() {
-        return appointmentTime;
-    }
-
-    public void setAppointmentTime(String appointmentTime) {
-        this.appointmentTime = appointmentTime;
-    }
-
-    public String getAppointmentStatus() {
-        return appointmentStatus;
-    }
-
-    public void setAppointmentStatus(String appointmentStatus) {
-        this.appointmentStatus = appointmentStatus;
-    }
-
-    public String getStatusLastModified() {
-        return statusLastModified;
-    }
-
-    public void setStatusLastModified(String statusLastModified) {
-        this.statusLastModified = statusLastModified;
-    }
-
-    public String getStatusChangeComment() {
-        return statusChangeComment;
-    }
-
-    public void setStatusChangeComment(String statusChangeComment) {
-        this.statusChangeComment = statusChangeComment;
-    }
 }
--- api/src/main/java/com/example/appointment/dao/AppointmentRepository.java
+++ api/src/main/java/com/example/appointment/dao/AppointmentRepository.java
@@ -1,11 +1,16 @@
 package com.example.appointment.dao;
 
+<<<<<<< HEAD
 import io.micronaut.data.annotation.Repository;
+=======
+import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
+import io.micronaut.data.mongodb.annotation.MongoRepository;
+>>>>>>> f67e42c (MongoDb implementation)
 import io.micronaut.data.repository.PageableRepository;
 import java.util.List;
 
-//@MongoRepository
-@Repository
+@MongoRepository
 public interface AppointmentRepository extends PageableRepository<Appointment, String> {
 
     //Page<Appointment> findAllByUserId(String userId, Pageable page);
